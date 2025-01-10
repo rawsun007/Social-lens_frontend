d310f177eefd2812ed3702e4c55e1607375e30bd
rawsun007 <roshanramani.dev@gmail.com>
2025-01-10 22:09:00 +0530

    layout update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
+import Message from "./Message"; // Import the Message component
 import "../index.css";
 import "../App.css";
 
@@ -9,19 +10,16 @@ function Chat() {
   const [isTyping, setIsTyping] = useState(false);
   const chatWindowRef = useRef(null);
 
-  // Handle the send message functionality
   const handleSendMessage = async (e) => {
     e.preventDefault();
 
-    // Prevent sending an empty message
     if (!userInput.trim()) return;
 
-    // User's message handling
     const userMessage = { sender: "user", text: userInput };
     setMessages((prevMessages) => [...prevMessages, userMessage]);
 
     setUserInput(""); // Clear the input field
-    setIsTyping(true); // Show "Bot is typing..."
+    setIsTyping(true);
 
     try {
       const response = await axios.post(
@@ -38,18 +36,16 @@ function Chat() {
       const botMessage = { sender: "bot", text: botResponse };
       setMessages((prevMessages) => [...prevMessages, botMessage]);
     } catch (error) {
-      // If there is an error fetching from the bot, show this message
       const errorMessage = {
         sender: "bot",
         text: "Apologies for the inconvenience, but we’re currently unable to retrieve a response from the bot. We deeply appreciate your patience and kindly ask you to try again shortly. Thank you for your understanding.",
       };
       setMessages((prevMessages) => [...prevMessages, errorMessage]);
     } finally {
-      setIsTyping(false); // Hide "Bot is typing..."
+      setIsTyping(false);
     }
   };
 
-  // Auto scroll to the bottom when new messages are added
   useEffect(() => {
     if (chatWindowRef.current) {
       chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
@@ -70,12 +66,7 @@ function Chat() {
           </div>
           <div className="messages-container" ref={chatWindowRef}>
             {messages.map((message, index) => (
-              <div
-                key={index}
-                className={message.sender === "user" ? "user" : "bot"}
-              >
-                {message.text}
-              </div>
+              <Message key={index} sender={message.sender} text={message.text} />
             ))}
             {isTyping && <div className="bot rounded">Bot is typing...</div>}
           </div>
@@ -88,7 +79,7 @@ function Chat() {
                 placeholder="Type your message..."
                 className="chat-input text-lg"
               />
-              <button type="submit" className="send-btn ">
+              <button type="submit" className="send-btn">
                 Send
               </button>
             </form>
